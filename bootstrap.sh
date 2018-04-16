73afa34666407eb0355ff586254b2b74a25901b3
Sir_Boops <admin@boops.me>
2018-04-16 06:14:35 -0600

    Add dev and prod mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# See if we are running in prod or dev mode
+echo ""
+echo ""
+echo "Hello! Would you like to run in 'prod' or 'dev' mode? ( Type your answer without the ' )"
+echo ""
+echo "If you're not sure just type 'prod'"
+echo ""
+read -p "MODE: " RUN_MODE
+RUN_MODE=`echo $RUN_MODE | awk '{ print toupper($0) }'`
+
+# Make sure the user input something sane
+if [ $RUN_MODE != "PROD" ] && [ $RUN_MODE != "DEV" ]; then
+    echo ""
+    echo "You have to choose 'prod' or 'dev'!"
+    echo "Exiting, no changes made"
+    echo ""
+    exit 1
+fi
+
+# Update the compose file
+sed -i "s/%RUN_MODE%/$RUN_MODE/" docker-compose.yml
+
 # Grab the docker-pleroma docker file
 git clone https://git.sergal.org/Sir-Boops/docker-pleroma
 
@@ -9,9 +31,13 @@ docker build -t $PLEROMA_NAME docker-pleroma/
 
 # Generate and copy the config file out
 COND_NAME="pleroma_`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`"
-docker run -it --name $COND_NAME $PLEROMA_NAME ash -c 'cd /opt/pleroma && mix generate_config'
+docker run -it --name $COND_NAME $PLEROMA_NAME ash -c 'su - -s /bin/ash pleroma -c cd pleroma && mix generate_config'
 docker cp $COND_NAME:/opt/pleroma/config config
 docker rm $COND_NAME
+
+# Create the uploads dir
+mkdir uploads
+chown 1000:1000 uploads
 
 # Init the database
 DB_NAME="db_`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`"
@@ -30,4 +56,4 @@ docker rm $DB_NAME
 # Get the config ready
 sed -i '/password:/c\  password: "pleroma",' config/generated_config.exs
 sed -i '/hostname:/c\  hostname: "postgres",' config/generated_config.exs
-cp config/generated_config.exs config/dev.secret.exs
+cp config/generated_config.exs config/`echo $RUN_MODE | awk '{ print tolower($0) }'`.secret.exs
