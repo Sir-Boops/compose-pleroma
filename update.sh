0adfa13a151e4af2465b6663be22db886ad01fc4
Sir_Boops <admin@boops.me>
2018-07-01 12:58:23 -0600

    Pull don't build

--- update.sh
+++ update.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-cd docker-pleroma && git pull && cd ..
-
-if [ -d "docker-tor" ]; then
-    cd docker-tor && git pull && cd ..
-fi
-
-if [ -d "docker-privoxy" ]; then
-    cd docker-privoxy && git pull && cd ..
-fi
-
-docker-compose build
+docker-compose pull
 docker-compose down
 docker-compose up -d
